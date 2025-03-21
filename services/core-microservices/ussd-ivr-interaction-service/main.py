5e75f6f6373d11dc4a1a803e4204f86c495b222d
MissTipo <dorine.a.tipo@gmail.com>
2025-03-21 11:47:42 +0300

    Removed core-microservices folder
    Signed-off-by: MissTipo <dorine.a.tipo@gmail.com>

--- services/core-microservices/ussd-ivr-interaction-service/main.py
+++
@@ -1,9 +0,0 @@
-# FastAPI app
-
-from fastapi import FastAPI
-from ussd_handler import ussd_handler
-
-app = FastAPI()
-
-# Mount the USSD endpoint to the FastAPI app
-app.add_route("/api/ussd", ussd_handler, methods=["GET"])
