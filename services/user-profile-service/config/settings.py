fb0a161cce7911a4c3baa759291e801522e39f29
MissTipo <dorine.a.tipo@gmail.com>
2025-04-16 17:31:07 +0300

    fix(settings): add DATABASE_URL string to fix validation error for Settings
    Signed-off-by: MissTipo <dorine.a.tipo@gmail.com>

--- services/user-profile-service/config/settings.py
+++ services/user-profile-service/config/settings.py
@@ -4,7 +4,7 @@ import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    DATABASE_URL: str
+    DATABASE_URL: str = "sqlite:///./test_org.db"
     model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
