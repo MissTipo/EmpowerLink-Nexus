441f592be0c8c14e1b5b11d48139651cb47469b3
MissTipo <dorine.a.tipo@gmail.com>
2025-05-07 15:20:37 +0300

    fix(telephony): point USSD callback to /ussd endpoint instead of GraphQL
    - Changed USSD_CALLBACK_URL in settings to use public /ussd route
    - Ensured getUSSDMenu resolver posts to the USSD HTTP handler, not /graphql
    - Eliminates 400 Bad Request errors when simulating USSD flows
    
    Signed-off-by: MissTipo <dorine.a.tipo@gmail.com>

--- services/telephony-integration-service/config/settings.py
+++ services/telephony-integration-service/config/settings.py
@@ -7,8 +7,8 @@ class Settings(BaseSettings):
     IVR_NUMBER: str = "300"
     # The GraphQL endpoint of the user profile service. Adjust as needed.
     USER_PROFILE_GRAPHQL_URL: str = "http://159.203.54.10.nip.io/graphql"
-    # USSD_CALLBACK_URL: str = "http://telephony-integration-service:8000/ussd"
-    USSD_CALLBACK_URL: str = "http://159.203.54.10.nip.io/ussd"
+    USSD_CALLBACK_URL: str = "http://telephony-integration:8003/graphql/"
+    # USSD_CALLBACK_URL: str = "http://159.203.54.10.nip.io/ussd"
 
     
     model_config = SettingsConfigDict(env_file=".env")
