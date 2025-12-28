3a69b76ce27b64e701b85e4a49e6767173dc0b45
MissTipo <dorine.a.tipo@gmail.com>
2025-12-28 19:18:45 +0300

    fix(telephony): point GraphQL and USSD callbacks to production domain
    Signed-off-by: MissTipo <dorine.a.tipo@gmail.com>

--- services/telephony-integration-service/config/settings.py
+++ services/telephony-integration-service/config/settings.py
@@ -3,9 +3,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     USSD_CODE: str = "*999#"
     IVR_NUMBER: str = "300"
-    USER_PROFILE_GRAPHQL_URL: str = "http://159.203.54.10.nip.io/graphql"
-    GEOMAP_URL: str = "http://159.203.54.10.nip.io/graphql"
-    USSD_CALLBACK_URL: str = "http://159.203.54.10.nip.io/ussd"
+    USER_PROFILE_GRAPHQL_URL: str = "https://empowerlinknexus.me/graphql"
+    GEOMAP_URL: str = "https://empowerlinknexus.me/graphql"
+    USSD_CALLBACK_URL: str = "https://empowerlinknexus.me/ussd"
 
     
     model_config = SettingsConfigDict(env_file=".env")
